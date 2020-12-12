26ee50bef906528aab7d746e50795acb65a6cb01
Server Admin <server-admin@casjaysdev.com>
2020-12-12 18:58:46 -0500

    🦈🏠🐜❗ Initial Commit ❗🐜🦈🏠

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header />
+    <GithubExplorer />
+  </div>
+</template>
+
+<script>
+import GithubExplorer from "./components/GithubExplorer.vue";
+import Header from "./components/Header.vue";
+export default {
+  name: "app",
+  components: {
+    GithubExplorer,
+    Header,
+  },
+};
+</script>
+
+<style lang="less">
+@import "../node_modules/bootswatch/dist/darkly/bootstrap.min.css";
+@import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+#app {
+  background: #eeeeee;
+  min-height: 100vh;
+}
+</style>
